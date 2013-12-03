8a1ec7793bff8e998cd80f239ced26cac21fd27c
Thomas Keuleers <thomas_keuleers@hotmail.com>
2013-12-03 18:33:56 +0100

    Added Outputtypes controller for selecting different kind of codes (not used yet)

---
+++ iOS7-NativeBarCodeScanning/SelectOutputTypesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectOutputTypesViewController.h
+//  iOS7-NativeBarCodeScanning
+//
+//  Created by Thomas Keuleers on 01/12/13.
+//  Copyright (c) 2013 Thomas Keuleers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectOutputTypesViewController : UIViewController
+
+@end
---
+++ iOS7-NativeBarCodeScanning/SelectOutputTypesViewController.m
@@ -0,0 +1,38 @@
+//
+//  SelectOutputTypesViewController.m
+//  iOS7-NativeBarCodeScanning
+//
+//  Created by Thomas Keuleers on 01/12/13.
+//  Copyright (c) 2013 Thomas Keuleers. All rights reserved.
+//
+
+#import "SelectOutputTypesViewController.h"
+
+@interface SelectOutputTypesViewController ()
+
+@end
+
+@implementation SelectOutputTypesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
